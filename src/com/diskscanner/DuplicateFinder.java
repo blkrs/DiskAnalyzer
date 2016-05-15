45336fa016c250e4f640b401814db75448d22944
Krzysztof Balka <krzysztof.balka@gmail.com>
2016-05-15 18:29:37 +0200

    Calculating redundant disk space usage

--- src/com/diskscanner/DuplicateFinder.java
+++ src/com/diskscanner/DuplicateFinder.java
@@ -16,8 +16,10 @@ import java.util.*;
 public class DuplicateFinder {
 
     private static DuplicateFinder _instance = null;
+    private long duplicatedSpace = 0;
     Map<FileInfo, List<String>> totalMap = new HashMap<>();
     Set<FileInfo> duplicates = new HashSet<>();
+    Set<String> processed = new HashSet<>();
 
     private DuplicateFinder() {};
 
@@ -39,27 +41,33 @@ public class DuplicateFinder {
     }
 
     public void countDuplicates() {
-        for ( FileInfo info : duplicates) {
-            //System.out.println("Duplicate found:");
+        for (FileInfo info : duplicates) {
             for (String filePath1 : totalMap.get(info)) {
                 try {
                     checkFile(info, filePath1);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                processed.add(filePath1);
             }
         }
+        System.out.println("Total redundant space: " + duplicatedSpace + " bytes");
     }
 
     private void checkFile(FileInfo info, String filePath1) throws IOException {
         //System.out.println("Checking file: " + filePath1);
         for (String filePath2 : totalMap.get(info)) {
+            if (processed.contains(filePath2)) continue;
             if (!filePath1.equals(filePath2)  &&
                     (quickSum(filePath1) == quickSum(filePath2)) &&
             (md5(filePath1).equals(md5(filePath2)))) {
-                System.out.println("Duplicate files: " + filePath1 + " ," + filePath2);
+                duplicatedSpace += info.getSize();
+                if (info.getSize() > 1000000) {
+                    System.out.println("Duplicate files: " + filePath1 + " ," + filePath2);
+                }
             }
          }
+
     }
 
 
@@ -83,8 +91,10 @@ public class DuplicateFinder {
     }
 
     public void clear() {
+        processed.clear();
         duplicates.clear();
         totalMap.clear();
+        duplicatedSpace = 0;
     }
 
 }
